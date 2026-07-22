23cb6a591f8eb94f9bc5963aa15dd64ff6d773aa
Alfred <donbenci13@users.noreply.github.com>
2026-07-22 07:36:37 +0000

    Initial commit

---
+++ test/pearlmagnet.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Pearlmagnet from '../lib/pearlmagnet-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pearlmagnet-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Pearlmagnet.PearlmagnetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
